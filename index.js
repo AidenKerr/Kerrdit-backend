27ed48dfd18918cb2552538cafe338a1a8ed8e62
AidenKerr <arkerr2000@gmail.com>
2021-07-02 22:19:39 -0700

    Add Login functionality

--- index.js
+++ index.js
@@ -1,8 +1,73 @@
 const express = require('express');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const dotenv = require('dotenv');
+const mysql = require("mysql");
+
+// initialize
+dotenv.config();
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: process.env.DB_PASSWORD,
+    database: "kerrdit"
+});
 const app = express();
+app.use(cors());
+app.use(express.json());
+const saltRounds = 10;
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.send("Kerrdit Backend");
+})
+
+app.post('/api/login', (req, res) => {
+    
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const errorMessage = "Log In Error";
+
+    // bcrypt.hash(password, saltRounds, function(err, hash) {
+    //     console.log(hash)
+    // });
+
+
+    db.query('SELECT password FROM users WHERE username = ?;', username, (err, result) => {
+        if (err) throw err;
+
+        if (! result[0]) {
+            res.status(404).send(errorMessage);
+            return;
+        }
+
+        const hash = result[0].password;
+
+        bcrypt.compare(password, hash, function(error, result) {
+            if (error) throw error;
+            
+            if (result) {
+                // login success
+                res.status(200).json({
+                    result: username
+                });
+            } else {
+                // login fail
+                res.status(404).send(errorMessage);
+            }
+    
+        });
+    });
+})
+
+app.get('/users', (req, res) => {
+    db.query("SELECT * FROM users", (err, result) => {
+        if (err) {
+            res.status(400).json(err);
+        } else {
+            res.status(200).json(result);
+        }
+    });
 })
 
 app.listen(3001, () => {
